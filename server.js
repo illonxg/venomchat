e15fd8d8d7bae02ba06cbf97f90865c7e10fc81a
456637789illa <illonxg@gmail.com>
2025-04-06 17:49:46 +0300

    1

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const server = http.createServer((req,res)=>{
         case '/script.js': return res.end(scriptFile)
         case '/style.css': return res.end(styleFile)
     }
-    if(req.url == '/'){
-        return res.end(indexHtmlFile)
-    }
-    res.statusCode = 404;
+    
+    res.statusCode == 404;
     return res.end('Error 404')
 });
 server.listen(3000);
