79af0bf7b02c6277c275b04721869f4de8c25f01
456637789illa <illonxg@gmail.com>
2025-04-06 17:47:43 +0300

    1

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'))
 const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'))
 const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'))
+
 const server = http.createServer((req,res)=>{
     switch(req.url) {
         case '/': return res.end(indexHtmlFile)
