558531e122f38e2b7ab102322ec75609205f3f26
456637789illa <illonxg@gmail.com>
2025-04-27 17:29:43 +0300

    1

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
-
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'))
-const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'))
-const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'))
-
-const server = http.createServer((req,res)=>{
+const db = require('./database');
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const server = http.createServer((req, res) => {
     switch(req.url) {
-        case '/': return res.end(indexHtmlFile)
-        case '/script.js': return res.end(scriptFile)
-        case '/style.css': return res.end(styleFile)
+        case '/': return res.end(indexHtmlFile);
+        case '/script.js': return res.end(scriptFile);
+        case '/style.css': return res.end(styleFile);
     }
-    
-    res.statusCode == 404;
-    return res.end('Error 404')
+    return res.end('Error 404');
 });
 server.listen(3000);
-
 const { Server } = require("socket.io");
 const io = new Server(server);
-
-io.on('connection', (socket) => {
-    console.log('a user connected. id - ' + socket.id);
-    let userNickname = 'user';
-    socket.on('set_nickname', (nickname) => {
-      userNickname = nickname;
-    });
-    socket.on('new_message', (message) => {
-      io.emit('message', userNickname + ' : ' + message);
-    });
+io.on('connection', async (socket) => {
+  console.log('a user connected. id - ' + socket.id);
+  let userNickname = 'admin';
+  let messages = await db.getMessages();
+  socket.emit('all_messages', messages);
+  socket.on('new_message', (message) => {
+    db.addMessage(message, 1);
+    io.emit('message', userNickname + ' : ' + message);
   });
-  
-  
+});
