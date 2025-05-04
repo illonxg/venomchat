343f9b10a09c72e653ad11a2f21c5402d4a371a2
456637789illa <illonxg@gmail.com>
2025-05-04 17:44:14 +0300

    1

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const db = require('./database');
 const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'));
 const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'));
 const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'));
+const registerFile = fs.readFileSync(path.join(__dirname, 'static', 'register.html'));
+const authFile = fs.readFileSync(path.join(__dirname, 'static', 'auth.js'));
+
 const server = http.createServer((req, res) => {
     switch(req.url) {
         case '/': return res.end(indexHtmlFile);
         case '/script.js': return res.end(scriptFile);
         case '/style.css': return res.end(styleFile);
+        case '/register': return res.end(registerFile)
+        case '/auth': return res.end(authFile)
     }
     return res.end('Error 404');
 });
