b4db8d14992a46d32c28ed1070d9be221eedded4
456637789illa <illonxg@gmail.com>
2025-05-11 17:48:19 +0300

    1

--- database.js
+++ database.js
@@ -56,10 +56,32 @@ module.exports = {
       console.error(dbError);
     }
   },
+
   addMessage: async (msg, userId) => {
     await db.run(
       `INSERT INTO message (content, autor) VALUES (?, ?)`,
       [msg, userId]
     );
+  },
+  isUserExist: async (login) => {
+    const candidate = await db.all(`SELECT * FROM user WHERE login = ?`, [login]);
+    return !!candidate.length;
+  },
+  addUser: async (user) => {
+    await db.run(
+      `INSERT INTO user (login, password) VALUES (?, ?)`,
+      [user.login, user.password]
+    );
+  },
+  
+  getAuthtoken: async(user) =>{
+    const candidate = await db.all(`SELECT * FROM user WHERE login =?`, [user.login]);
+    if(!candidate.length){
+      throw 'Wrong login';
+    }
+    if(candidate[0].password !== user.password){
+      throw 'Wrong password';
+    }
+    return candidate[0].user_id + '.' + candidate[0].login + '.' + crypto.randomBytes(20).toString('hex');
   }
 };
